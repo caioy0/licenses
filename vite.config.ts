4c337dac745cb7eab5fe02df42fdae1e8f4c2ac2
caio <caio.yugo@proton.me>
2026-07-03 00:32:26 -0300

    feat: cloudflare deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 
 function figmaAssetResolver() {
@@ -23,6 +24,7 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    cloudflare(),
   ],
   resolve: {
     alias: {
